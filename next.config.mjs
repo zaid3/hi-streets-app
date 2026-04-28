544bdf07db73be3d683910b52139e3300627f794
A Haque <40199643+zaid3@users.noreply.github.com>
2026-04-28 02:26:02 +0100

    Configure Next.js for standalone output and environment vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+}
 export default nextConfig

485b181fa6256b3f7339e9a82948284b4c96a685
A Haque <40199643+zaid3@users.noreply.github.com>
2026-05-07 00:29:09 +0100

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-}
-export default nextConfig

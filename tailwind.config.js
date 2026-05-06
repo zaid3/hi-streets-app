5f4726876fec3a5b97b2c1b6ea06e88aca5bb835
A Haque <40199643+zaid3@users.noreply.github.com>
2026-05-07 00:29:35 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-  ],
-  theme: { extend: {} },
-  plugins: [],
-}

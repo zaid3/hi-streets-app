b7e9915cfc3c5059b6416b6f7880e72cef649c18
A Haque <40199643+zaid3@users.noreply.github.com>
2026-07-12 14:13:39 +0100

    Import clean UI CSS layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
+import './histreets-ui.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

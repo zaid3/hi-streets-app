6a59c6f5fd284f0a32595bef5fb2fa39a9a33c8e
A Haque <40199643+zaid3@users.noreply.github.com>
2026-07-16 19:05:54 +0100

    Import final polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
 import './histreets-ui.css'
+import './final-polish.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(

bc48920fa0593bd9165f7ccbb40c7b40fee219d0
A Haque <40199643+zaid3@users.noreply.github.com>
2026-08-24 11:27:50 +0100

    Cache final iPhone install icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'histreets-shell-v4'
-const APP_SHELL = ['/manifest.json', '/icon.svg', '/icon-192.svg', '/icon-512.svg']
+const CACHE_NAME = 'histreets-shell-v5'
+const APP_SHELL = ['/manifest.json', '/icon.svg', '/icon-192.svg', '/icon-512.svg', '/apple-touch-icon.png']
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)))

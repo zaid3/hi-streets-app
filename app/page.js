cd4479257b9375d41a1d02f69524262137901796
A Haque <40199643+zaid3@users.noreply.github.com>
2026-08-24 14:34:29 +0100

    Remove obsolete Next.js application shell

--- app/page.js
+++
@@ -1,8 +0,0 @@
-'use client'
-import{useEffect}from'react'
-import{useRouter}from'next/navigation'
-export default function Root(){
-  const r=useRouter()
-  useEffect(()=>{r.replace('/map')},[])
-  return<div style={{height:'100dvh',background:'#0a0a0a'}}/>
-}
